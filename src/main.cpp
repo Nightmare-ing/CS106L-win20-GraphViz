e3b9c46ce7ebaa07ca010d8d68948815fee463dd
Qingsong Hu <kingsanhu@gmail.com>
2023-03-03 16:47:43 +0800

    Finish Implementation Details 1
    1. Finished the prompt user for the name function
    2. Modified the project setting to run in terminal, otherwise can't get
       user input from console

--- src/main.cpp
+++ src/main.cpp
@@ -3,15 +3,22 @@
  */
 #include <iostream>
 #include "SimpleGraph.h"
+#include <fstream>
 
 using namespace std;
 
 void Welcome();
+void openFile(ifstream& file);
 
 // Main method
 int main() {
     Welcome();
     /* TODO: your implementation here */
+    ifstream file;
+    openFile(file);
+    int a;
+    file >> a;
+    cout << a << endl;
     return 0;
 }
 
@@ -22,6 +29,23 @@ void Welcome() {
     cout << "This program uses a force-directed graph layout algorithm" << endl;
     cout << "to render sleek, snazzy pictures of various graphs." << endl;
     cout << endl;
+}
+
+void openFile(ifstream& file) {
+    string fileName;
+    while (true) {
+        if (!getline(cin, fileName)) {
+            cout << "can't read the file name, please check!" << endl;
+        }
+        file.open(fileName);
+        if (file.is_open()) {
+            return;
+        } else {
+            cout << "can't open the file, please check!" << endl;
+            file.clear(); // clear the bad bit status for fstream
+        }
+    }
+
 }
 
 
